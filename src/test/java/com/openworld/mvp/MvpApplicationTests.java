cd8ca23922ab229939963f551ad4061bae2fc164
maxxrl <m.wesener@gmx.de>
2021-12-30 23:21:58 +0100

    Initial commit.

---
+++ src/test/java/com/openworld/mvp/MvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openworld.mvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
