a27faa5ff8413115a2431721e236bf389bf8ed4b
maxxrl <m.wesener@gmx.de>
2022-01-13 21:27:13 +0100

    Implemented first use case getWalletBalance.

---
+++ README.md
@@ -0,0 +1,18 @@
+#MVP Project Etherum Smart Contract Open World
+###@author maxxrl
+
+---
+Requirements
+* Java 17
+* Maven
+* Intellj as IDE
+* Ganache
+
+Setup
+1) Open ganache
+2) Check port matches test setup connection
+3) Run Tests by clicking play
+---
+Use Cases
+1) Receive wallet balance
+
--- src/test/java/com/openworld/mvp/MvpApplicationTests.java
+++ src/test/java/com/openworld/mvp/MvpApplicationTests.java
@@ -1,13 +1,41 @@
 package com.openworld.mvp;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameter;
+import org.web3j.protocol.core.methods.response.EthGetBalance;
+import org.web3j.protocol.http.HttpService;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 @SpringBootTest
 class MvpApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void getWalletBalance() throws ExecutionException, InterruptedException, TimeoutException {
+        final String ganachePort = "7545";
+        final String localEtherAPI = "http://localhost:" + ganachePort;
+        final String localEtherWalletAddress = "0x9c98b4c8090d27eFB4125596A982f35D5d50F717";
+        final Web3j client = Web3j.build(new HttpService(localEtherAPI));
+        final EthGetBalance clientResponse =
+                client
+                        .ethGetBalance(localEtherWalletAddress, DefaultBlockParameter.valueOf("latest"))
+                        .sendAsync()
+                        .get(5, TimeUnit.SECONDS);
+
+        final BigDecimal conversionValue = new BigDecimal(1000000000000000000L);
+
+        final BigInteger unscaledBalance = clientResponse.getBalance();
+        final BigDecimal scaledBalance = new BigDecimal(unscaledBalance).divide(conversionValue, 2, RoundingMode.HALF_UP);
+        System.out.println("Scaled local wallet balance " + scaledBalance + "ETH");
+        Assertions.assertThat(scaledBalance).isEqualTo(new BigDecimal("100.00"));
+    }
 
 }
