a525b4e5c014532d6973a95d130f3f80350677d9
maxxrl <m.wesener@gmx.de>
2022-01-25 20:26:15 +0100

    Added cors support

--- src/main/java/com/openworld/mvp/security/SpringSecurityConfig.java
+++ src/main/java/com/openworld/mvp/security/SpringSecurityConfig.java
@@ -1,20 +1,46 @@
 package com.openworld.mvp.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private Environment env;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        String localOriginFrontend = env.getProperty("local.origin.frontend");
+        String remoteOriginFrontend = env.getProperty("remote.origin.frontend");
+
+        List<String> origins = new ArrayList<>();
+
+        if ((localOriginFrontend != null
+                && !localOriginFrontend.isEmpty())
+                &&
+                (remoteOriginFrontend != null && !remoteOriginFrontend.isEmpty())
+        ) {
+            origins.add(remoteOriginFrontend);
+            origins.add(localOriginFrontend);
+        }
+
+        http
+                .cors().configurationSource(request -> {
+                    CorsConfiguration cors = new CorsConfiguration();
+                    cors.setAllowedOriginPatterns(origins);
+                    cors.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                    cors.setAllowedHeaders(List.of("*"));
+                    cors.setAllowCredentials(true);
+                    return cors;
+                }).and()
+                .csrf()
+                .disable();
         http
                 .authorizeRequests()
                 .antMatchers("/api/v1/abc/**")
